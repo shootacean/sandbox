e5bb1c6d4c1c8613c64f7e89a19fbbf155fd234d
Shootacean <shootacean@gmail.com>
2019-01-22 22:52:31 +0900

    [add] golang gin  ORM Sqlite3

--- golang/gin/main.go
+++ golang/gin/main.go
@@ -1,12 +1,48 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+type Keyword struct {
+    gorm.Model
+    Id   int
+    Name string
+}
+
+func initDB() {
+    db, err := gorm.Open("sqlite3", "gin.sqlite")
+    if err != nil {
+        panic("failed to connect database\n")
+    }
+    db.AutoMigrate(&Keyword{})
+}
+
+func getAll() []Keyword {
+    db, err := gorm.Open("sqlite3", "gin.sqlite")
+    if err != nil {
+        panic("failed to connect database\n")
+    }
+    var keyword []Keyword
+    db.Find(&keyword)
+    return keyword
+    
+}
 
 func main() {
     r := gin.Default()
+    initDB()
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
+        })
+    })
+    r.GET("/keywords", func(c *gin.Context) {
+        keyword := getAll()
+        c.JSON(200, gin.H{
+            "keywords": keyword,
         })
     })
     r.Run()
