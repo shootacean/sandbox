669a77e7509762e15812fe72c80636a632c8c878
Shota Tsukamoto <s-tsukamoto@tsgroup.jp>
2019-05-13 19:00:12 +0900

    [add] switch process by firebase env #firebase-env

--- firebase-env/functions/src/index.ts
+++ firebase-env/functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as firebase from 'firebase-admin'
+
+firebase.initializeApp({});
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    if (functions.config().env.id === 'prod') {
+        response.send("Hello from Firebase Production!");
+    } else {
+        response.send("Hello from Firebase Development!");
+    }
 });
